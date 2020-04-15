f2600405845e1cb4c5ebd7a57c5ff3e82cd51378
ryan98100 <c14180115@john.petra.ac.id>
2020-04-15 09:23:58 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,23 @@ export class AppComponent  {
   name = 'Angular';
   input1="";
   input2="";
+  pola="";
   data=0;
-  output="Hasil: ";
-  hitung(){
-  this.data = parseInt(this.input1) * parseInt(this.input2);
-  this.output += this.data;
+  line = "*";
+  hitung(data1,data2){
+  this.data = Math.pow(data1,data2);
+  if (this.data % 2 == 0){
+  this.pola = "Pola B";
+  const result = [];
+  for(var i=0;i<this.data;i++){
+    result.push(`${this.line}\n`)
+    this.line +=" *";
   }
+  }
+  else{
+  this.pola = "Pola A";
+  }
+  }
+
 
 }
