c8c1c549b96b43d793d1d9bf728c49d45498ed34
ryan98100 <c14180115@john.petra.ac.id>
2020-04-15 10:12:06 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,59 @@ export class AppComponent  {
   input2="";
   pola="";
   data=0;
+  star="";
   line = "*";
+  result ="";
+  htmlstring="";
+  enol="";
+  lmao="0";
+  temp=0;
   hitung(data1,data2){
+  this.htmlstring="";
+  this.temp= data2;
   this.data = Math.pow(data1,data2);
   if (this.data % 2 == 0){
   this.pola = "Pola B";
-  const result = [];
-  for(var i=0;i<this.data;i++){
-    result.push(`${this.line}\n`)
-    this.line +=" *";
+  for(var o=0;o<data2-1;o++){
+   this.line += "*";
   }
+  for(var i=data1;i<this.data;i++){
+    this.result+= this.line;
+    this.result += "<br>";
+    this.line+= "*";
+  }
+  this.htmlstring=this.result;
+  this.result="";
+  this.line="*";
   }
   else{
   this.pola = "Pola A";
+  for(var i=0;i<data2;i++){
+    this.star="";
+    for(var j=this.temp;j>0;j--){
+      this.star += this.line;
+    }
+    this.temp--;
+    if(i < data1){
+      this.enol += this.lmao;
+      this.result += this.star;
+      this.result +=this.enol;
+      this.result += "<br>"
+    }
+    else{
+      this.result += this.star;
+      this.result += "<br>"
+    }
+  }
+  this.htmlstring=this.result;
+  this.result="";
+  this.enol="";
+  this.lmao="o";
+  this.line="*";
   }
   }
-
-
+  reset(){
+    this.result="";
+    this.htmlstring="";
+  }
 }
