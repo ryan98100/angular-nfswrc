db6a21549faaad506fed1108f9492b3cdc08f3ed
ryan98100 <c14180115@john.petra.ac.id>
2020-04-29 08:31:55 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
 
-import{Routes ,RouterModule} from '@angular/router'
-const Routes:Routes = [
-  {path:'/home' , component:HomeComponent}
-]
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(Routes) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
